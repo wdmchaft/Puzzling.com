86c63a1476f709b5e441a08c82952d0988406fdc
Jonathan Tilley <jtilley>
2012-04-10 02:05:41 -0700

    Added a starter for api calling in javascript

---
+++ api.js
@@ -0,0 +1,24 @@
+//TILLEY: a super quick example of how to do a api call using ajax.  It needs
+//to be a lot better designed that this, so it can handle multiple requests, etc
+
+var xmlhttp;
+var callback;
+
+function getUser(uid, callback){
+    //an example api calling method in javascript
+    if (window.XMLHttpRequest){
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else{// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function(){
+        if (xmlhttp.readyState==4 && xmlhttp.status==200){
+            callback(eval(xmlhttp.responseText));
+        }
+    }
+    xmlhttp.open("POST","our/website/url",true);
+    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    xmlhttp.send("uid="+uid);
+}
