5b9b024df1eb74ce8b5f571a141b397dc8a6a5df
Jonathan Tilley <jtilley>
2012-04-11 12:06:39 -0700

    javascript api updated to work generically, now just all the api calls we want need to be added.

--- api.js
+++ api.js
@@ -1,24 +1,50 @@
-//TILLEY: a super quick example of how to do a api call using ajax.  It needs
-//to be a lot better designed that this, so it can handle multiple requests, etc
+//TILLEY:much better, will work with any number of calls made in sequence
 
-var xmlhttp;
-var callback;
-
-function getUser(uid, callback){
+function APIHandler(url,params, callback){
+    this.callback = callback;
+    this.params = params;
+    this.url = url;
     //an example api calling method in javascript
     if (window.XMLHttpRequest){
-    // code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp=new XMLHttpRequest();
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        this.xmlhttp=new XMLHttpRequest();
     }
     else{// code for IE6, IE5
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+        this.xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
     }
-    xmlhttp.onreadystatechange=function(){
-        if (xmlhttp.readyState==4 && xmlhttp.status==200){
-            callback(eval(xmlhttp.responseText));
-        }
-    }
-    xmlhttp.open("POST","our/website/url",true);
+    xmlhttp.onreadystatechange = this.wrap(this,"ajaxResponse");
+}
+
+APIHandler.prototype.start = function(){
+    xmlhttp.open("POST",this.url,true);
     xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-    xmlhttp.send("uid="+uid);
+    xmlhttp.send(this.params);
+}
+
+APIHandler.prototype.ajaxResponse = function() {
+    if (this.xmlhttp.readyState==4){
+        if(this.xmlhttp.status==200){
+            this.callback(eval(this.xmlhttp.responseText));
+        }else{
+            this.callback("Error" + this.xmlhttp.status + " on api call to " + this.url + " with parameters " + this.params);
+        }
+    }
+}
+
+
+APIHandler.prototype.wrap = function(obj, method) {
+    return function() {
+        obj[method]();
+    }
+}
+
+
+function getUser(uid, callback){
+    var handler = new APIHandler("our/site/url/getUser", "uid="+uid, callback);
+    handler.start();
+}
+
+function getPuzzle(puzzleid, callback){
+    var handler = new APIHandler("our/site/url/getUser", "puzzzleid="+puzzleid, callback);
+    handler.start();
 }
