364369df14650e81ce74bc001e2424b3bd379a12
Jonathan Tilley <jtilley>
2012-05-02 14:43:07 -0700

    fixing memory leaks in puzzle operations, added puzzle object

---
+++ SDKs/ios/PuzzleSDK/PuzzleSDK/Classes/SDK/Public/Puzzle Models/PuzzlePuzzle.h
@@ -0,0 +1,13 @@
+//
+//  PuzzlePuzzle.h
+//  PuzzleSDK
+//
+//  Created by Jonathan Tilley on 5/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PuzzlePuzzle : NSObject
+
+@end
---
+++ SDKs/ios/PuzzleSDK/PuzzleSDK/Classes/SDK/Public/Puzzle Models/PuzzlePuzzle.m
@@ -0,0 +1,13 @@
+//
+//  PuzzlePuzzle.m
+//  PuzzleSDK
+//
+//  Created by Jonathan Tilley on 5/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PuzzlePuzzle.h"
+
+@implementation PuzzlePuzzle
+
+@end
