deda755211ce6b7154d7459eb6a340614bdc8ce8
Jonathan Tilley <jtilley>
2012-06-10 11:33:01 -0700

    fresh api key

---
+++ TowerTactics/PuzzleAPIKey.h
@@ -0,0 +1,11 @@
+//
+//  PuzzleAPIKey.h
+//  PuzzleSDK
+//
+//  Created by Jonathan Tilley on 4/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+//#define API_KEY @"4fb047047e94b047a9000002" //localhost
+//#define API_KEY @"4fc41da7026df61c41000003" //server
+#define API_KEY @"4fd4e6f1bde7c6bc6c029e7f" //tower tactics
